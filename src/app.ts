ddaf3dd243b6dc133ce397e699adb04fdd72de8b
juliana <julianaacademi@gmail.com>
2026-08-24 17:45:44 -0300

    feat: integrate Swagger for API documentation and add new routes

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
 import express from "express";
 import type { Express } from "express";
-import userRoutes from "./routes/routes.js";
+
 import { logger } from "./middlewares/logger.middleware.js";
 import swaggerUi from "swagger-ui-express";
+import authRoutes from "./routes/login.route.js";
+import userRoutes from "./routes/user.route.js";
+import courseRoutes from "./routes/course.route.js";
+import enrollmentRoutes from "./routes/enrollment.route.js";
+import { swaggerSpec } from "./swagger.js";
 
 const app: Express = express();
 
@@ -10,8 +15,11 @@ app.use(express.json());
 
 app.use(logger);
 
-app.use("/", userRoutes);
+app.use(authRoutes);
+app.use(userRoutes);
+app.use(courseRoutes);
+app.use(enrollmentRoutes);
 
-
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 export default app;
